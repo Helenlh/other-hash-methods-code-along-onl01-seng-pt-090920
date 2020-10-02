65a5b38e9b7e97f3ed83a07a5f458d274f77b909
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-02 23:29:02 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -11,7 +11,7 @@ require 'pry'
 
 def get_the_values(groceries) 
   #code your solution here! 
-  
-  groceries.values 
-  groceries.to_s 
-end  
+category =groceries.keys 
+  items = groceries.values
+  puts items 
+  end   
